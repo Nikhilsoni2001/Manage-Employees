2533c751d8a38039adad90f3285f4bbc2fea1637
Nikhil <nsoni19012001@gmail.com>
2022-09-28 23:54:18 +0530

    Add Configuration and Homepage

---
+++ employee-system-api/src/test/java/com/nikhil/employee/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
